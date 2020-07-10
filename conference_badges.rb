9aa28d68de5696ef54265eb507499c548396714d
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-10 23:40:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badges)
+result = []
+badges.each do |badge|
+  result << badge_maker(badge)
+end
+result
+end
+
+def assign_rooms(badges)
+result = []
+  badges.each_with_index do |name, num|
+result << "Hello, #{name}! You'll be assigned to room #{num + 1}!"
+end
+result
+end
+
+def printer (badges)
+  batch_badge_creator(badges).each do |badges|
+  puts assign_rooms
+end
+end
